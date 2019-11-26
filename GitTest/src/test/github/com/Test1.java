860488537893aac20ce875dab2b9b6553a562f1d
董乃士 <d1321225602>
2019-11-26 09:28:03 +0800

    master功能

---
+++ GitTest/src/test/github/com/Test1.java
@@ -0,0 +1,32 @@
+
+package test.github.com;
+
+/**
+ 	* <p>
+ 	* Title:test.github.com
+ 	* </p>
+ 	* <p/>
+ 	* <p>
+ 	* Description: 描述【Git测试】
+ 	* </p>
+ 	* <p/>
+	* <p>
+ 	* Copyright: Copyright (c) 2019
+ 	* </p>
+ 	* <p/>
+ 	* <p>
+ 	* Company:太原科技大学
+ 	* </p>
+ 	*
+ 	* @author 董乃士
+ 	* @version 1.0
+ 	* @created  2019年11月26日 上午9:17:29
+ 	*/
+
+public class Test1 {
+	public static void main(String []args) {
+		System.out.println("这是master");
+	}
+
+
+}
